dbc627779a6c21da6cb97e7c06f9305bfc4e6133
Andrii Novitskyi <novitskiyandrew@gmail.com>
2017-05-09 14:17:50 +0200

    Project created. Dispatcher servlet and WelcomeController created.

---
+++ src/main/java/controllers/WelcomeController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController
+{
+
+}
