00b41fe910ae2b06ac19c08abfd20bbb6d1b9923
SebastianCiba <sebastian.ciba@o2.pl>
2017-05-20 20:17:53 +0200

    Add files via upload

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -1,18 +1,15 @@
-package controllers;
+package car.controllers;
 
 //import dao.CarDaoTemporaryImpl;
-import model.Car;
-import model.CarForm;
+import car.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import service.CarService;
+import car.service.CarService;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,7 +17,7 @@ import java.util.List;
 public class WelcomeController
 {
     @Autowired
-    CarService service;
+    CarService carService;
 
     @RequestMapping("/")
     public String mainPage()
@@ -31,7 +28,7 @@ public class WelcomeController
     @RequestMapping("/display")
     public String showAll(Model model)
     {
-        List<Car> list = service.getAll();
+        List<Car> list = carService.getAll();
         if (!list.isEmpty())
             model.addAttribute("list", list);
         else
@@ -39,29 +36,31 @@ public class WelcomeController
         return "display";
     }
 
-    @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public String search(Model model)
-    {
-        model.addAttribute("car", new CarForm("", "", 0));
-        model.addAttribute("list", service.getSortedList());
-        return "search";
-    }
+//    @RequestMapping(value = "/search", method = RequestMethod.GET)
+//    public String search(Model model)
+//    {
+//        model.addAttribute(new Car(/*CarDaoTemporaryImpl.getCount()*/0, "", "", 0, 0));
+//        List<Car> list = service.getAll();
+//        Collections.sort(list, (a, b) -> a.getCompany().compareTo(b.getCompany()));
+//        model.addAttribute("list", list);
+//        return "search";
+//    }
 
-    @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public String search(@Valid CarForm car, BindingResult result, Model model)
-    {
-        if (result.hasErrors())
-        {
-            model.addAttribute("list", service.getSortedList());
-            return "search";
-        }
-        Car c = new Car(car.getCompany(), car.getModel(), car.getYear(), 0);
-        List<Car> list = service.search(c);
-        if (list.size() > 0)
-            model.addAttribute("list", list);
-        else
-            model.addAttribute("error", "There is no available cars that match to your require. Try once more.");
-        return "display";
-    }
-
+//    @RequestMapping(value = "/search", method = RequestMethod.POST)
+//    public String search(@Valid Car car, BindingResult result, Model model)
+//    {
+//        if (result.hasErrors())
+//        {
+//            List<Car> list = service.getAll();
+//            Collections.sort(list, (a, b) -> a.getCompany().compareTo(b.getCompany()));
+//            model.addAttribute("list", list);
+//            return "search";
+//        }
+//        List<Car> list = service.search(car);
+//        if (list.size() > 0)
+//            model.addAttribute("list", list);
+//        else
+//            model.addAttribute("error", "There is no available cars that match to your require. Try once more.");
+//        return "display";
+//    }
 }
